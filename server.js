6d9f4abd9129f18e0e5c268a433446a86df7c5f3
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-02-19 15:20:02 -0800

    connected to database

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 9000;
+const mongoose = require('mongoose');
 const cors = require('cors');
+const {db_connect} = require('./config');
+const authRoutes = require('./routes/auth');
 
-const app = express()
+const app = express();
+
+mongoose.connect(db_connect).then(()=> {
+    console.log('database is connected');
+}).catch(err=> {
+    console.log('error connecting to the database', err.message);
+})
+
+app.use(cors());
+app.use(express.json());
+
+authRoutes(app);
 
 app.get('/', (req, res) => {
-    console.log('reached endpoint')
-    res.send('<h1>Empty</h1>')
+    res.send('<h1>The end is nigh\'</h1>')
 })
+
 
 app.listen(PORT, ()=> {
     console.log('the system is running on port:', PORT);
